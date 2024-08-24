870c6eaf747bcadd41c1779818c8eb196c7dcaad
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-24 17:37:12 +0500

    Initial Commit

---
+++ Swift-Navigation/Swift_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_NavigationApp.swift
+//  Swift-Navigation
+//
+//  Created by Naveed on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
